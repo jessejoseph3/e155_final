fd61d697f922f7564625cee955936db1ad2bc937
jessejoseph3 <jjoseph@hmc.edu>
2015-11-23 00:28:47 -0800

    added read_png line

--- final/edge-find.c
+++ final/edge-find.c
@@ -12,8 +12,8 @@ void main(int argc, char *argv[])
 	int exitStatus = loadimage(file, png_ptr, info_ptr);
 	printf("image loaded? %d \n",exitStatus);
 
-	png_read_png(png_ptr, info_ptr, png_transforms, NULL);
-	
+	png_read_png(png_ptr, info_ptr, 0, NULL);
+
 	png_byte bitDepth 	= 	png_get_bit_depth(png_ptr, info_ptr);
 	png_byte colorType	=	png_get_color_type(png_ptr, info_ptr);
 	int width 	=	png_get_image_width(png_ptr, info_ptr);
