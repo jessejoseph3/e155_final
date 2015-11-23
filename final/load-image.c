f746c1b05faf209aef6aac8fd35d6206798ae978
jessejoseph3 <jjoseph@hmc.edu>
2015-11-23 05:53:37 -0800

    nope didnt

--- final/load-image.c
+++ final/load-image.c
@@ -82,7 +82,7 @@ void write_png_file(char *filename, char *image_one_dim) {
  	int x,y;
  	for(y = 0; y < height; y++){
  		for(x = 0; x < width; x++){
- 			px[0] = (png_byte) image_one_dim[width*y + x];
+ 			px[0] = (char) image_one_dim[width*y + x];
  			row[x] = (png_bytep) px;
  		}
  		row_pointers[y] = row;
