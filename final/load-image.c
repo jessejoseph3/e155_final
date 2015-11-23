33c8c33fe988a8246bae760013d4b4a89161c330
jessejoseph3 <jjoseph@hmc.edu>
2015-11-23 05:29:47 -0800

    fixed pointer for reconstruction maybe

--- final/load-image.c
+++ final/load-image.c
@@ -77,7 +77,7 @@ void write_png_file(char *filename, char *image_one_dim) {
   //png_set_filler(png, 0, PNG_FILLER_AFTER);
 
  	png_bytep row_pointers[height];
- 	png_bytep px[1];
+ 	char px[1];
  	png_bytep row[width];
  	int x,y;
  	for(y = 0; y < height; y++){
