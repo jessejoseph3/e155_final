fc84b07d5c99e33b4cc38e403503d8becd8fbb9d
jessejoseph3 <jjoseph@hmc.edu>
2015-11-23 04:10:26 -0800

    scope or something

--- final/compute-edges.c
+++ final/compute-edges.c
@@ -2,9 +2,7 @@
 
 const double threshold = 0.3;
 
-char * find_edges_grayscale(png_structp png_ptr,
-						 png_infop info_ptr,
-						 png_bytep *row_pointers)
+char * find_edges_grayscale(png_bytep *row_pointers)
 {
 	edges = (char *) malloc(height*width);
 	char valueToWrite;
@@ -37,11 +35,12 @@ char * find_edges_grayscale(png_structp png_ptr,
     return edges;
 }
 
-double average_difference(int x, int y, int c, int pxsize)
+double average_difference(int x, int y,
+						int c, int pxsize,
+						png_bytep *row_pointers)
 {
 	int i, j;
-	int sum;
-	int average = 0.0;
+	double average = 0.0;
 	for(i = -1; i < 2; i++){
 		png_bytep row = row_pointers[y + i];
 		for(j = -1; j < 2; j++){
@@ -61,7 +60,8 @@ double average_difference(int x, int y, int c, int pxsize)
 }
 
 
-double average_diff_of_image(int c, int pxsize)
+double average_diff_of_image(int c, int pxsize,
+							png_bytep *row_pointers)
 {
 	double numPixels = height*width;
 	double average = 0.0;
