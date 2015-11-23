1f44a2edc92909af18916e0885b37f6f8fa843cc
jessejoseph3 <jjoseph@hmc.edu>
2015-11-23 00:11:59 -0800

    added lines to get image info

--- final/edge-find.c
+++ final/edge-find.c
@@ -13,9 +13,13 @@ void main(int argc, char *argv[])
 
 	int bitDepth 	= 	png_get_bit_depth(png_ptr, info_ptr);
 	int colorType	=	png_get_color_type(png_ptr, info_ptr);
+	int width 		=	png_get_width(png_ptr, info_ptr);
+	int height		=	png_get_height(png_ptr, info_ptr);
 
 	printf("bitDepth: %d \n", bitDepth);
 	printf("colorType: %d \n", colorType);
+	printf("width: %d \n", width);
+	printf("height: %d \n", height);
 
 	//char **row_pointers;
 
