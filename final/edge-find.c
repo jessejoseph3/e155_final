0ea198303c99e62b69a982bf2492d3414b180409
jessejoseph3 <jjoseph@hmc.edu>
2015-11-22 22:40:37 -0800

    apparently i hadnt even added edge-find

--- final/edge-find.c
+++ final/edge-find.c
@@ -1,9 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "imgproc.h"
+#include "load-image.c"
 
+static png_structp png_ptr;
+static png_infop info_ptr;
 
 void main(int argc, char *argv[])
 {
-	
+	loadimage('obama.png');
+
+	int bitDepth 	= 	png_get_bit_depth(png_ptr, info_ptr);
+	int colorType	=	png_get_color_type(png_ptr, info_ptr);
+
+	printf(bitDepth);
+	printf(colorType);
+
+	//char **row_pointers;
+
+	//png_read_png(png_ptr, info_ptr, png_transforms, NULL);
+	//row_pointers = png_get_rows(png_ptr, info_ptr);
+
+
 }
