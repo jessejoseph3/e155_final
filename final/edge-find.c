43f307f4be2487ff9099072e6b7cc10769e92a01
jessejoseph3 <jjoseph@hmc.edu>
2015-11-22 23:03:23 -0800

    how do strings work

--- final/edge-find.c
+++ final/edge-find.c
@@ -4,13 +4,14 @@
 
 static png_structp png_ptr;
 static png_infop info_ptr;
+const char * file = 'obama.png';
 
 void main(int argc, char *argv[])
 {
-	loadimage('obama.png', png_ptr, info_ptr);
+	loadimage(file, png_ptr, info_ptr);
 
-	int bitDepth 	= 	png_get_bit_depth(png_ptr, info_ptr);
-	int colorType	=	png_get_color_type(png_ptr, info_ptr);
+	int *bitDepth 	= 	png_get_bit_depth(png_ptr, info_ptr);
+	int *colorType	=	png_get_color_type(png_ptr, info_ptr);
 
 	printf(bitDepth);
 	printf(colorType);
